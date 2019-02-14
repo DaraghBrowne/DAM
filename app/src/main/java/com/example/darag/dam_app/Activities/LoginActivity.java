b54b03740c542c818aba59c1d57d1a15f49b8ffb
DaraghBrowne <C15396706@mydit.ie>
2019-02-14 15:24:54 +0000

    Login activity working

--- app/src/main/java/com/example/darag/dam_app/Activities/LoginActivity.java
+++ app/src/main/java/com/example/darag/dam_app/Activities/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.example.darag.dam_app.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.darag.dam_app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,14 +21,33 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private Button regButton;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        userEmail = findViewById(R.id.loginEmail);
+        userPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
 
+        mAuth = FirebaseAuth.getInstance();
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
 
+                if(email.isEmpty() || password.isEmpty()){
+                    showMessage("Please fill in all fields.");
+                }
+                else{
+                    signIn(email,password);
+                }
+            }
+        });
 
         regButton= findViewById(R.id.loginRegButton);
 
@@ -35,5 +60,30 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(intent);
             }
         });
+    }
+
+    private void signIn(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //if successful bring user to home page
+                    Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(homeActivity);
+                    finish();
+                }
+                else{
+                    showMessage("login error" + task.getException().getMessage());
+                }
+            }
+        });
+    }
+
+    //toast messages method
+    private void showMessage(String s) {
+
+        Toast.makeText(getApplicationContext(),s, Toast.LENGTH_LONG).show();
+
     }
 }
