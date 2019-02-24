c14a514abe66c36a97c8192376309b8c9d90256b
DaraghBrowne <C15396706@mydit.ie>
2019-02-24 22:38:35 +0000

    Finished main Nav and logout

--- app/src/main/java/com/example/darag/dam_app/Activities/HomeActivity.java
+++ app/src/main/java/com/example/darag/dam_app/Activities/HomeActivity.java
@@ -1,8 +1,17 @@
 package com.example.darag.dam_app.Activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.darag.dam_app.Fragments.BreatheFragment;
+import com.example.darag.dam_app.Fragments.CRFragment;
+import com.example.darag.dam_app.Fragments.HomeFragment;
+import com.example.darag.dam_app.Fragments.MoodsFragment;
+import com.example.darag.dam_app.Fragments.SettingsFragment;
 import com.example.darag.dam_app.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -11,5 +20,51 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //reference to our bottom navigation view
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+
+        //pass listener to our bottom navigation
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //HomeFragment is displayed when we open the app
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
     }
+
+    //react to clicks on the items
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null; //this var will hold reference to the fragment we want to open
+
+                    //find out by the item id which item was clicked
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_moods:
+                            selectedFragment = new MoodsFragment();
+                            break;
+                        case R.id.nav_cr:
+                            selectedFragment = new CRFragment();
+                            break;
+                        case R.id.nav_breathe:
+                            selectedFragment = new BreatheFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+
+                    //to show the fragments we created above ^
+                    //pass container where we want to display our fragment in, and fragment we want to show
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true; //true means we want to select the clicked item
+                }
+            };
 }
---
+++ app/src/main/java/com/example/darag/dam_app/Fragments/CRFragment.java
@@ -0,0 +1,19 @@
+package com.example.darag.dam_app.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.darag.dam_app.R;
+
+public class CRFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cr,container,false);
+    }
+}
