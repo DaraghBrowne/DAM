cdc95d8e05704f30c65170b90d18a768e7eaeb61
DaraghBrowne <C15396706@mydit.ie>
2019-03-10 22:11:12 +0000

    Added displaying name for welcome

--- app/src/main/java/com/example/darag/dam_app/HomeFragments/HomeFragmentTab1.java
+++ app/src/main/java/com/example/darag/dam_app/HomeFragments/HomeFragmentTab1.java
@@ -7,15 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.darag.dam_app.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragmentTab1 extends Fragment {
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String name = user.getDisplayName();
+    TextView textViewUserName;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         //specify layout we want to show for the fragment
-        return inflater.inflate(R.layout.fragment_home_tab1,container,false);
+        View view = inflater.inflate(R.layout.fragment_home_tab1,container,false);
+
+        textViewUserName = view.findViewById(R.id.welcome_text);
+        textViewUserName.setText("Welcome "+ name);
+
+        return view;
+
+
     }
 }
